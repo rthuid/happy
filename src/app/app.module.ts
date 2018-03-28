362fb3553831a43631cd2771e71340a73abd3622
rthuid <rthuid@gmail.com>
2018-03-28 21:57:07 +0530

    left menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CarouselComponent } from './components/carousel/carousel.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './layout/footer/footer.component';
+import { LeftMenusComponent } from './layout/left-menus/left-menus.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { FooterComponent } from './layout/footer/footer.component';
     NotificationComponent,
     MainSearchComponent,
     CarouselComponent,
-    FooterComponent
+    FooterComponent,
+    LeftMenusComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/left-menus/left-menus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-menus',
+  templateUrl: './left-menus.component.html',
+  styleUrls: ['./left-menus.component.scss']
+})
+export class LeftMenusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
