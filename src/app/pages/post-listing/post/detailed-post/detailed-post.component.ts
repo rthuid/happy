677b37c69274ce7e64cd529757fed0b856129a0d
rthuid <rthuid@gmail.com>
2018-04-08 23:50:03 +0530

    detailed page

--- src/app/pages/post-listing/post/detailed-post/detailed-post.component.ts
+++ src/app/pages/post-listing/post/detailed-post/detailed-post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ElementRef } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-detailed-post',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detailed-post.component.scss']
 })
 export class DetailedPostComponent implements OnInit {
-
-  constructor() { }
+  closeResult: string;
+  toggleCommented = false;
+  likeCount = 12;
+  toggleLike = false;
+  liketext = ' Like';
+  toggleFollowed = false;
+  followtext = ' Follow';
+  constructor(private modalService: NgbModal, private elRef: ElementRef) {}
 
   ngOnInit() {
+  }
+  // toggleCommented
+  toggleComment() {
+    this.toggleCommented = !this.toggleCommented;
+    setTimeout(() => {
+      this.elRef.nativeElement.querySelector('textarea.mainComment').focus();
+    }, 100);
+  }
+  // like
+  likeFun() {
+    this.toggleLike = !this.toggleLike;
+    if (this.toggleLike === true) {
+      this.likeCount = this.likeCount + 1;
+      this.liketext = ' Liked';
+    } else {
+      this.likeCount = this.likeCount - 1;
+      this.liketext = ' Like';
+    }
+  }
+  followFun() {
+    this.toggleFollowed = !this.toggleFollowed;
+    if (this.toggleFollowed === true) {
+      this.followtext = ' Followed';
+    } else {
+      this.followtext = ' Follow';
+    }
+  }
+
+  // modals
+  openSm(content) {
+    this.modalService.open(content, { size: 'sm' });
   }
 
 }
